5cc76df9c9921590d3f32a49695082b8260b47ef
mo7ammedfadhil <73299647+mo7ammedfadhil@users.noreply.github.com>
2021-08-12 18:57:01 +0100

    Add files via upload

---
+++ AllScripts/FindDifferenceBetweenDuration.m
@@ -0,0 +1,18 @@
+% Return the the total difference in seconds between packets
+function [outputArg1] =  FindDifferenceBetweenDuration(inputArg)
+
+format long
+    CaptureTime=inputArg(1);
+    totalTime=CaptureTime-CaptureTime;
+      for i=1:size(inputArg)
+            CurrentTime=inputArg(i);  
+            TimeAsDuration(i)=CurrentTime-CaptureTime; 
+            totalTime=TimeAsDuration(i)+totalTime;
+      end
+         tt=split(string(totalTime),":") ;
+         outputArg1=str2num(tt(3));
+end
+
+
+
+
